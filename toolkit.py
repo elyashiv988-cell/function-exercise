522e27ec3dc1b4c4823d3c613a28349770ae90a1
Elyashiv <elyashiv988@gmail.com>
2026-06-28 14:04:52 +0300

    added 4

---
+++ toolkit.py
@@ -0,0 +1,19 @@
+# part 1
+# 1
+def greet(name):
+    print("hello",name)
+greet("agent x")
+# 2
+def add(a,b):
+    print(a+b)
+add(3,4)
+# 3
+def square(n):
+    print(n**2)
+square(5)
+square(12)
+# 4
+def greet_with_title(name, title="Agent"):
+    print("hello",title,name)
+greet_with_title(name="eli")
+greet_with_title("eli","student")
